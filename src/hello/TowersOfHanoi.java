2a49a4356cf54cb75d5e2be117af2294d9569bed
articuly <articuly@gmail.com>
2021-03-16 09:46:45 +0800

    递归，汉诺塔

---
+++ src/hello/TowersOfHanoi.java
@@ -0,0 +1,41 @@
+package hello;
+/*
+  目的：实现汉诺塔问题求解
+  作者：Dmego  时间：2016-10-15
+ */
+
+import java.util.Scanner;
+
+public class TowersOfHanoi {
+    static int m = 0; //标记移动次数
+
+    //实现移动的函数
+    public static void move(int disks, char N, char M) {
+        System.out.println("第" + (++m) + "次移动：把" + disks + "号圆盘从" + N + "->移到->" + M);
+    }
+
+    //递归实现汉诺塔的函数
+    public static void hanoi(int n, char A, char B, char C) {
+        if (n == 1) {
+            TowersOfHanoi.move(1, A, C);
+        } else {
+            hanoi(n - 1, A, C, B);//递归，把A塔上编号1~n-1的圆盘移动B上，以C显辅助塔
+            TowersOfHanoi.move(n, A, C);//把A塔上编号为n的圆盘移到C上
+            hanoi(n - 1, B, A, C);//递归，把B塔上1~n-1的圆盘移动C上，以A为辅助塔
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        char A = 'A';
+        char B = 'B';
+        char C = 'C';
+        System.out.println("******************************************************************************************");
+        System.out.println("这是汉诺塔问题（把A塔上编号从小号到大号的圆盘从A塔通过B辅助塔移动到C塔上去");
+        System.out.println("******************************************************************************************");
+        System.out.print("请输入圆盘的个数：");
+        int disks = input.nextInt();
+        TowersOfHanoi.hanoi(disks, A, B, C);
+        System.out.println(">>移动了" + m + "次，把A上的圆盘都移动到了C上");
+    }
+}
