e812df087743082462f845641e7b8c5e0ef1929b
logan_lin <loganlin@aslcn.com.cn>
2021-04-23 10:28:55 +0800

    class practise

--- src/classes/ATM.java
+++ src/classes/ATM.java
@@ -1,5 +1,7 @@
 package classes;
 
+import java.util.Objects;
+
 public class ATM {
     private double money = 0;
 
@@ -11,6 +13,26 @@ public class ATM {
         this.money += cash;
         System.out.println("you have saved " + cash);
         System.out.println("your balance is " + this.money);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ATM atm = (ATM) o;
+        return Double.compare(atm.money, money) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(money);
+    }
+
+    @Override
+    public String toString() {
+        return "ATM{" +
+                "money=" + money +
+                '}';
     }
 
     void withdrawCash(double cash) {
