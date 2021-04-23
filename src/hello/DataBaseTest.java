e014d2bce27e4cf847b6c4e433c3f59ff6c14407
logan_lin <loganlin@aslcn.com.cn>
2021-04-23 10:28:43 +0800

    database test

---
+++ src/hello/DataBaseTest.java
@@ -0,0 +1,63 @@
+package hello;
+
+import java.sql.*;
+
+public class DataBaseTest {
+    // MySQL 8.0 以下版本 - JDBC 驱动名及数据库 URL
+    static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
+    static final String DB_URL = "jdbc:mysql://localhost:3306/test";
+    // MySQL 8.0 以上版本 - JDBC 驱动名及数据库 URL
+    //static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
+    //static final String DB_URL = "jdbc:mysql://localhost:3306/test?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
+
+    // 数据库的用户名与密码，需要根据自己的设置
+    static final String USER = "root";
+    static final String PASS = "123456";
+
+    public static void main(String[] args) {
+        Connection conn = null;
+        Statement stmt = null;
+        try {
+            // 注册 JDBC 驱动
+            Class.forName(JDBC_DRIVER);
+            // 打开链接
+            System.out.println("连接数据库。。。");
+            conn = DriverManager.getConnection(DB_URL, USER, PASS);
+            // 执行查询
+            System.out.println(" 实例化Statement对象...");
+            stmt = conn.createStatement();
+            String sql;
+            sql = "select id, name from users";
+            // DML操作用stmt.executeUpdate()
+            ResultSet rs = stmt.executeQuery(sql);
+            // 展开结果集数据库
+            while (rs.next()) { // 替代器rs.next()
+                // 通过字段检索
+                int id = rs.getInt("id");
+                String name = rs.getString("name");
+                // 输出数据
+                System.out.print("ID:" + id + " ");
+                System.out.print("名称:" + name);
+                System.out.print("\n");
+            }
+            // 完成后关闭
+            rs.close();
+            stmt.close();
+            conn.close();
+        } catch (SQLException se) {
+            se.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (stmt != null) stmt.close();
+            } catch (SQLException se2) {
+            }
+            try {
+                if (conn != null) conn.close();
+            } catch (SQLException se3) {
+            }
+        }
+        System.out.println("Goodbye");
+    }
+}
